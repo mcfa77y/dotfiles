c0868f4d433b55a9a7883e93bbb1df012eaabf3b
Joe Lau <joe@empohealth.com>
2026-06-16 12:28:01 -0700

    fix: resolve zshrc sourcing issues for OMZ and Zinit configurations

--- zsh/custom/ngrok.zsh
+++ zsh/custom/ngrok.zsh
@@ -1,5 +1,5 @@
 # from brew upgrade 2034-10-31 happy halloween!
-cache_completion ngrok
+cache_completion ngrok _ngrok "SHELL=/bin/zsh ngrok completion"
 
 alias ngrok_start='ngrok http --domain=saved-heron-driving.ngrok-free.app 3000'
 alias ngrok_start2='ngrok http --domain=saved-heron-driving.ngrok-free.app https://localhost:3002'
