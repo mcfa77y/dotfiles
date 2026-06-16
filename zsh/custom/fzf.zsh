238401bc6aa2cc359fbb27cba624e00fcd82b58e
Joe Lau <joe@empohealth.com>
2026-06-16 12:37:16 -0700

    refactor: move fzf cache completion setup to custom/fzf.zsh

--- zsh/custom/fzf.zsh
+++ zsh/custom/fzf.zsh
@@ -1,3 +1,7 @@
 fzf_multi_select() {
    fzf -m --preview "bat --color=always {}" --header "Select files (Tab to multi-select)"
  } 
+
+# fzf - useful keybindings and fuzzy completion
+cache_completion fzf fzf_init.zsh "cat /opt/homebrew/opt/fzf/shell/completion.zsh /opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+[[ -f ~/.zsh/completions/fzf_init.zsh ]] && source ~/.zsh/completions/fzf_init.zsh
