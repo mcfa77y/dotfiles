984dd2b957440c375c7060df2506b26b9e2f7270
Joe Lau <joe@empohealth.com>
2026-06-23 16:29:07 -0700

    zsh: add _fd_excludes helper function in fzf.zsh and refactor compgen functions to use it

--- zsh/custom/fzf.zsh
+++ zsh/custom/fzf.zsh
@@ -46,13 +46,20 @@ _fzf_comprun() {
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
+_fd_excludes() {
+  local item
+  for item in "$@"; do
+    echo -n "--exclude=$item "
+  done
+}
+
 _fzf_compgen_path() {
-  fd --hidden --follow --exclude ".git" --exclude "node_modules" --exclude "target" --exclude ".cache" . "$1"
+  fd --hidden --follow $(_fd_excludes .git node_modules target .cache) . "$1"
 }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-  fd --type d --hidden --follow --exclude ".git" --exclude "node_modules" --exclude "target" --exclude ".cache" . "$1"
+  fd --type d --hidden --follow $(_fd_excludes .git node_modules target .cache) . "$1"
 }
 
 fzf_multi_select() {
